125e05fba6598d438416d548c2cf9cf483cda3e8
tirth19 <cool.trb@gmail.com>
2020-06-13 08:50:13 +0530

    Added notification at 6:20 PM everyday (doesn't seem to be working)

---
+++ app/src/main/java/com/gyansaarthi/fastbook/AlarmReceiver.java
@@ -0,0 +1,45 @@
+package com.gyansaarthi.fastbook;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+
+import androidx.core.app.NotificationCompat;
+
+import com.gyansaarthi.fastbook.Home.HomeActivity;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+
+        long when = System.currentTimeMillis();
+        NotificationManager notificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Intent notificationIntent = new Intent(context, HomeActivity.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+                notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder mNotifyBuilder = new NotificationCompat.Builder(
+                context).setSmallIcon(R.drawable.fastbook_logo)
+                .setContentTitle("Alarm Fired")
+                .setContentText("Events to be Performed").setSound(alarmSound)
+                .setAutoCancel(true).setWhen(when)
+                .setContentIntent(pendingIntent)
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000});
+        notificationManager.notify(1, mNotifyBuilder.build());
+
+    }
+
+}
